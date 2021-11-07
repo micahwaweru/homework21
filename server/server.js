5ae816719cdc09a036e54d4338076b14400d96c5
micahwaweru <micah.waweru@gmail.com>
2021-11-06 21:44:32 -0500

    server and installs

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const { ApolloServer } = require('./schemas');
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas')
 const { authMiddleware } = require('./utils/auth');
 
